6a543eafd14628099ae872c5102bb17db31bd3a2
235909 <245826@edu.p.lodz.pl>
2024-08-01 11:45:20 +0200

    Initial commit

---
+++ versions_info.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy
+import sklearn
+import matplotlib
+import pandas as pd
+
+print("NumPy version:", np.__version__)
+print("SciPy version:", scipy.__version__)
+print("scikit-learn version:", sklearn.__version__)
+print("matplotlib version:", matplotlib.__version__)
+print("pandas version:", pd.__version__)
+
+#NumPy version: 2.0.0
+#SciPy version: 1.14.0
+#scikit-learn version: 1.5.1
+#matplotlib version: 3.9.1
+#pandas version: 2.2.2
